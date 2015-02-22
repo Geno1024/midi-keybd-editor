1616a1112df028369d763b2e3937916c62c853d6
Y. Z. Chen <754097987@qq.com>
2015-02-22 17:13:00 +0800

    Initial commit

---
+++ src/com/geno/midikeybdeditor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.geno.midikeybdeditor;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+    }
+}
