f2c14df7ae057b4cb078ac32f6cebe86db037a96
Y. Z. Chen <754097987@qq.com>
2015-03-01 18:57:03 +0800

    add buttons

--- src/com/geno/midikeybdeditor/MainActivity.java
+++ src/com/geno/midikeybdeditor/MainActivity.java
@@ -7,10 +7,23 @@ import android.widget.*;
 
 public class MainActivity extends Activity
 {
+	public Button c,d,e,f,g,a,b,cr,dr,fr,gr,ar;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		c=(Button)findViewById(R.id.c);
+		d=(Button)findViewById(R.id.d);
+		e=(Button)findViewById(R.id.e);
+		f=(Button)findViewById(R.id.f);
+		g=(Button)findViewById(R.id.g);
+		a=(Button)findViewById(R.id.a);
+		b=(Button)findViewById(R.id.b);
+		cr=(Button)findViewById(R.id.cr);
+		dr=(Button)findViewById(R.id.dr);
+		fr=(Button)findViewById(R.id.fr);
+		gr=(Button)findViewById(R.id.gr);
+		ar=(Button)findViewById(R.id.ar);
     }
 }
