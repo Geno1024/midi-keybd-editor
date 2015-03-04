ccecdb360febafb41142fd5a4d9cd18c8bd7c5b4
Y. Z. Chen <754097987@qq.com>
2015-03-04 13:26:13 +0800

    add ids

--- src/com/geno/midikeybdeditor/MainActivity.java
+++ src/com/geno/midikeybdeditor/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.*;
 public class MainActivity extends Activity
 {
 	public Button c,d,e,f,g,a,b,cr,dr,fr,gr,ar;
+	public TextView expl,src;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -25,5 +26,7 @@ public class MainActivity extends Activity
 		fr=(Button)findViewById(R.id.fr);
 		gr=(Button)findViewById(R.id.gr);
 		ar=(Button)findViewById(R.id.ar);
+		expl=(TextView)findViewById(R.id.explanation);
+		src=(TextView)findViewById(R.id.srccode);
     }
 }
