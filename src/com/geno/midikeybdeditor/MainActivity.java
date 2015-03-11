a3883c4f0dd1e4cf3397109a7c9465d29dcee3c8
Y. Z. Chen <754097987@qq.com>
2015-03-11 13:27:33 +0800

    I found my note about MIDI yesterday night! So ALL the thought changed into a right way!!! And this is a big change!

--- src/com/geno/midikeybdeditor/MainActivity.java
+++ src/com/geno/midikeybdeditor/MainActivity.java
@@ -7,70 +7,57 @@ import android.view.*;
 import android.view.View.*;
 import android.widget.*;
 import java.nio.*;
+import java.util.*;
 
 public class MainActivity extends Activity
 {
-	public Button c,d,e,f,g,a,b,cr,dr,fr,gr,ar;
-	public TextView expl,src,detail;
+	public TextView expl,src,detail,addevent;
 	public byte[] music;
-	public String[] expla,timelength;
-	public String n64,n32,n16,n8,n4,n2,n1,note,remain,use;
+	public String[] expla,selectevent;
+	public String note,remain,use,event;
 	public StringBuffer sb;
 	public ByteBuffer midi;
+	public static int notelength;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
 	//	Get string res
-		n64 = getString(R.string.n64);
-		n32 = getString(R.string.n32);
-		n16 = getString(R.string.n16);
-		n8 = getString(R.string.n8);
-		n4 = getString(R.string.n4);
-		n2 = getString(R.string.n2);
-		n1 = getString(R.string.n1);
 		note = getString(R.string.note);
 		remain = getString(R.string.remain);
 		use = getString(R.string.nowuse);
-		
-		timelength = new String[] {n64,n32,n16,n8,n4,n2,n1};
+		event = getString(R.string.event);
+		selectevent = new String[]{getString(R.string.eventid8)};
+
 	//	Get layout id
-		c = (Button)findViewById(R.id.c);
-		d = (Button)findViewById(R.id.d);
-		e = (Button)findViewById(R.id.e);
-		f = (Button)findViewById(R.id.f);
-		g = (Button)findViewById(R.id.g);
-		a = (Button)findViewById(R.id.a);
-		b = (Button)findViewById(R.id.b);
-		cr = (Button)findViewById(R.id.cr);
-		dr = (Button)findViewById(R.id.dr);
-		fr = (Button)findViewById(R.id.fr);
-		gr = (Button)findViewById(R.id.gr);
-		ar = (Button)findViewById(R.id.ar);
 		expl = (TextView)findViewById(R.id.explanation);
 		src = (TextView)findViewById(R.id.srccode);
 		detail = (TextView)findViewById(R.id.detail);
+		addevent = (Button)findViewById(R.id.addevent);
+
 	//	Get byte buffer for editing
 		sb = new StringBuffer();
 		midi = ByteBuffer.allocate(1048576);
-		byte[] head = {0x4D,0x54,0x68,0x64,0x0,0x0};
+		byte[] head = {0x4D,0x54,0x68,0x64,0x0,0x0,0x0,0x6};
 		midi.put(head);
 		detail.setText(remain+midi.remaining()+use+midi.position());
+
 	//	Edit widget
-		c.setOnClickListener
+		addevent.setOnClickListener
 		(new OnClickListener()
 			{
 				@Override
 				public void onClick(View p1)
 				{
-					AlertDialog.Builder ad=new AlertDialog.Builder(MainActivity.this).setTitle(note).setItems
-					(timelength, new DialogInterface.OnClickListener()
+					AlertDialog.Builder ad=new AlertDialog.Builder(MainActivity.this).setTitle(event).setItems
+					(selectevent, new DialogInterface.OnClickListener()
 						{
 							@Override
 							public void onClick(DialogInterface p1, int p2)
 							{
-								mainloop(90,40);
+								eventchk(p2);
 							}
 						}
 					);
@@ -80,12 +67,9 @@ public class MainActivity extends Activity
 		);
     }
 
-	void mainloop(int note,int length)
+	void eventchk(int eventid)
 	{
 		
-		midi.put(Byte.parseByte(String.valueOf(note)));
-		midi.put(Byte.parseByte(String.valueOf(length)));
-		detail.setText(remain+midi.remaining()+use+midi.position());
 	}
 }
 
