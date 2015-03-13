b955e987e14f72f4a92a012b5643a0fb07822daf
Y. Z. Chen <754097987@qq.com>
2015-03-13 13:38:22 +0800

    Many new and rewritten functions, hard to think ubtosb without a paper.

--- src/com/geno/midikeybdeditor/MainActivity.java
+++ src/com/geno/midikeybdeditor/MainActivity.java
@@ -12,17 +12,18 @@ import java.util.*;
 public class MainActivity extends Activity
 {
 	public TextView expl,src,detail,addevent;
-	public byte[] music;
 	public String[] expla,selectevent;
-	public String note,remain,use,event,confirm,cancel;
+	public String note,remain,use,event,confirm,cancel,velocity;
 	public StringBuffer sb;
 	public ByteBuffer midi;
 	public static int notelength;
 	
 	//	These value are for functions
-	public byte notevalue;
+	public byte eventvalue,notevalue,notelengthvalue;
+	public byte[] eventdefinedvalue = {ubtosb(0x80)};
+	public ByteBuffer eventnotebuffer;
 	public String[] notedefinedname = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
-	public String[] note12 = {};
+	public String[] note12 = {"8x -5","8x -4","8x -3","8x -2","8x -1","8x ±0","8x +1","8x +2","8x +3","8x +4","8x +5"};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -37,6 +38,7 @@ public class MainActivity extends Activity
 		event = getString(R.string.event);
 		confirm = getString(R.string.confirm);
 		cancel = getString(R.string.cancel);
+		velocity = getString(R.string.velocity);
 		selectevent = new String[]{getString(R.string.eventid8)};
 
 	//	Get layout id
@@ -48,6 +50,7 @@ public class MainActivity extends Activity
 	//	Get byte buffer for editing
 		sb = new StringBuffer();
 		midi = ByteBuffer.allocate(1048576);
+		eventnotebuffer = ByteBuffer.allocate(3);
 		byte[] head = {0x4D,0x54,0x68,0x64,0x0,0x0,0x0,0x6};
 		midi.put(head);
 		detail.setText(remain+midi.remaining()+use+midi.position());
@@ -66,12 +69,6 @@ public class MainActivity extends Activity
 							public void onClick(DialogInterface p1, int p2)
 							{
 								eventchk(p2);
-								noteid();
-								if(notevalue != -1)
-								{
-									midi.put(notevalue);
-								}
-								update();
 							}
 						}
 					);
@@ -81,21 +78,23 @@ public class MainActivity extends Activity
 		);
     }
 
-	void eventchk(int eventid)
-	{
-		
-	}
-
 	void update()
 	{
-		detail.setText(remain+midi.remaining()+use+midi.position());
+		midi.put(eventnotebuffer);
+		detail.setText(ubtosb(0x80)+"@"+ubtosb(0xff)+remain+midi.remaining()+use+midi.position());
 	}
 
-	byte noteid()
+	void eventchk(int eventid)
+	{
+		eventnotebuffer.put(eventdefinedvalue[eventid]);
+		noteid();
+	}
+
+	void noteid()
 	{
 		notevalue = -1;
-		AlertDialog.Builder ad=new AlertDialog.Builder(MainActivity.this)
-		.setTitle(note)
+		AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this)
+		.setTitle(R.string.note)
 		.setItems
 			(notedefinedname,new DialogInterface.OnClickListener()
 				{
@@ -103,30 +102,31 @@ public class MainActivity extends Activity
 					public void onClick(DialogInterface p1, int p2)
 					{
 						notevalue = (byte) (0x3c + p2);
+						Toast.makeText(MainActivity.this,notevalue+"",Toast.LENGTH_SHORT).show();
+						if(notevalue!=-1)
+							eventnotebuffer.put(notevalue);
+						update();
+						notelength();
 					}
 				}
-			)
-/*		.setPositiveButton
-			(confirm,new DialogInterface.OnClickListener()
-				{
-					@Override
-					public void onClick(DialogInterface p1, int p2)
-					{}
-				}
-			)
-		.setNegativeButton
-			(cancel,new DialogInterface.OnClickListener()
-				{
-					@Override
-					public void onClick(DialogInterface p1, int p2)
-					{
-						notevalue = -1;
-					}
-				}
-			)*/;
+			);
 		ad.show();
-		Toast.makeText(MainActivity.this,notevalue+"",Toast.LENGTH_SHORT).show();
-		return notevalue;
+	}
+
+	void note12()
+	{
+	}
+
+	void notelength()
+	{
+		AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this).setTitle(R.string.velocity);
+		ad.show();
+	}
+
+	byte ubtosb(int unsigned)
+	{
+		//	Unsigned byte to signed byte
+		return unsigned < 128 ? (byte)unsigned : (byte)(128-unsigned);
 	}
 }
 
