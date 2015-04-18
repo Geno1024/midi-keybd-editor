32a6763f1e639a96a1bb56e92e98484524057185
Y. Z. Chen <754097987@qq.com>
2015-04-18 15:45:39 +0800

    Yahoo! import jar ok!

--- src/com/geno/midikeybdeditor/MainActivity.java
+++ src/com/geno/midikeybdeditor/MainActivity.java
@@ -10,6 +10,10 @@ import java.nio.*;
 import android.text.*;
 import java.io.*;
 import android.content.pm.*;
+import android.content.pm.PackageManager.*;
+import android.graphics.drawable.*;
+import android.widget.RelativeLayout.*;
+import android.graphics.*;
 
 public class MainActivity extends Activity
 {
@@ -97,7 +101,9 @@ public class MainActivity extends Activity
 
 	//	The func below are being tested
 		flag=1;
-		sysEx();
+		com.geno.necessity.About a = new com.geno.necessity.About();
+		a.about("",MainActivity.this);
+		//about("",this);
 
 	//	Edit widget
 		addevent.setOnClickListener
@@ -731,6 +737,41 @@ public class MainActivity extends Activity
 			}
 		)
 		.setNegativeButton(android.R.string.cancel,null);
+		ad.show();
+	}
+
+	void about(String inst,Context context)
+	{
+		PackageManager pm = context.getPackageManager();
+		LinearLayout layout = new LinearLayout(context);
+		ImageView appimage = new ImageView(context);
+		Drawable appdrawable = null;
+		TextView appname = new TextView(context);
+		TextView version = new TextView(context);
+
+		try
+		{
+			appdrawable=pm.getApplicationIcon(pm.getApplicationInfo(getPackageName(),pm.GET_META_DATA));
+			appname.setText(pm.getApplicationLabel(pm.getApplicationInfo(getPackageName(),pm.GET_META_DATA)));
+		}
+		catch (PackageManager.NameNotFoundException e){}
+
+		appimage.setImageDrawable(appdrawable);
+		appname.setTypeface(Typeface.MONOSPACE);
+		version.setText("\nBy Geno.\nY. Z. Chen\n\n"+inst);
+		version.setTypeface(Typeface.MONOSPACE);
+		version.setGravity(Gravity.CENTER);
+
+		layout.setOrientation(LinearLayout.VERTICAL);
+		layout.setGravity(Gravity.CENTER);
+		layout.addView(appimage,LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+		layout.addView(appname,LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+		layout.addView(version,LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+
+		AlertDialog.Builder ad = new AlertDialog.Builder(context)
+		.setTitle("About")
+		.setView(layout)
+		.setPositiveButton(android.R.string.ok,null);
 		ad.show();
 	}
 }
