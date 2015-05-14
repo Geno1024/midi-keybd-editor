73dd15d43b04893c6e2d69457fc4e26e85d2e65a
Y. Z. Chen <754097987@qq.com>
2015-05-14 13:17:53 +0800

    [add bug]no effect

--- src/com/geno/midikeybdeditor/FileIO.java
+++ src/com/geno/midikeybdeditor/FileIO.java
@@ -7,58 +7,8 @@ import java.io.*;
 
 public class FileIO extends Activity
 {
-	public byte[] out;
-	public byte[] open(final Context c)
-	{
-		final EditText t = new EditText(c);
-		AlertDialog.Builder ad = new AlertDialog.Builder(c)
-			.setTitle(R.string.open)
-			.setView(t)
-			.setPositiveButton
-		(android.R.string.ok,new DialogInterface.OnClickListener()
-			{
-				@Override
-				public void onClick(DialogInterface p1, int p2)
-				{
-					File f = new File(t.getText().toString());
-					InputStream i = null;
-					try
-					{
-						i = new BufferedInputStream(new FileInputStream(f));
-					}
-					catch(FileNotFoundException e)
-					{
-						Toast.makeText(c,R.string.filenotfound,Toast.LENGTH_SHORT).show();
-					}
-					try
-					{
-						i.read(out);
-						i.close();
-					}
-					catch(Exception e)
-					{
-						
-					}
-					/*
-					ByteBuffer b=null;
-					b = ByteBuffer.allocate((int)f.length());
-					try
-					{
-						i.read(b.array());
-						i.close();
-					}
-					catch (Exception e)
-					{}
-					*/
-					Toast.makeText(c,R.string.finish,Toast.LENGTH_SHORT).show();
-				}
-			}
-		)
-			.setNegativeButton(android.R.string.cancel,null);
-		ad.show();
-		return out;
-	}
-	/*public byte[] open(final Context c)
+	public byte[] r;
+	public final byte[] open(final Context c)
 	{
 		final EditText t = new EditText(c);
 		AlertDialog.Builder ad = new AlertDialog.Builder(c)
@@ -70,60 +20,29 @@ public class FileIO extends Activity
 				@Override
 				public void onClick(DialogInterface p1, int p2)
 				{
-					FileInputStream f = null;
-					try
-					{
-						f = new FileInputStream(t.getText().toString());
-						out = new byte[f.available()];
-						f.read(out);
-						f.close();
-					}
-					catch (Exception e)
-					{}
-					
-					/*
-					File f = new File(t.getText().toString());
-					InputStream i = null;
-					try
-					{
-						i = new BufferedInputStream(new FileInputStream(f));
-					}
-					catch(Exception e)
-					{
-						Toast.makeText(c,R.string.filenotfound,Toast.LENGTH_SHORT).show();
-						return;
-					}
-					try
-					{
-						i.read(toWrite);
-						i.close();
-					}
-					catch (Exception e)
-					{}
-					Toast.makeText(c,R.string.finish,Toast.LENGTH_SHORT).show();
+					r=openFileIntoByte(t.getText().toString());
 				}
 			}
 		)
 		.setNegativeButton(android.R.string.cancel,null);
 		ad.show();
-		return out;
-	}*/
-	/*public static byte[] open(String fileName) {
+		return r;
+	}
+
+	public static byte[] openFileIntoByte(String fileName)
+	{
+		byte[] out = null;
+		FileInputStream f = null;
 		try
 		{
-			FileInputStream in = new FileInputStream(fileName);
-			int length = in.available();
-			byte[] buffer = new byte[length];
-			in.read(buffer);
-			in.close();
-			byte[] content = EncodingUtils.getString(buffer, "UTF-8");
-			return content;
-        }
-		catch (Exception e)
-		{
-			com.geno.tools.Debug.toast((R.string.filenotfound),FileIO.this);
-			return new byte[]{};
-        }
-    }*/
+			f = new FileInputStream(fileName);
+			out = new byte[f.available()];
+			f.read(out);
+			f.close();
+		}
+		catch(Exception e)
+		{}
+		return out;
+	}
 }
 
